52e5b648e5750b781c22a2e9cf15d2ed93bc4084
jyotirmaya-sahoo <140313558+jyotirmaya-sahoo@users.noreply.github.com>
2024-08-30 08:57:39 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,3 +10,12 @@ def mavenbuild()
     sh 'mvn package'
 }
 
+def contdep(credential,privateip,pathname)
+{
+    deploy adapters: [tomcat9(credentialsId: '${credential}', path: '', url: 'http://${privateip}:8080')], contextPath: '${pathname}', war: '**/*.war'
+}
+
+
+
+
+
