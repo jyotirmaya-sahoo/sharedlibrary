fe88e54d435b8164d7ec4fdf657695ebb0d82d57
jyotirmaya-sahoo <140313558+jyotirmaya-sahoo@users.noreply.github.com>
2024-08-30 21:54:01 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,12 +10,6 @@ def mavenbuild()
     sh 'mvn package'
 }
 
-def contdep(credential,privateip,pathname)
-{
-     deploy adapters: [tomcat9(credentialsId: '${credential}', path: '', url: 'http://${privateip}:8080')], contextPath: '${pathname}', war: '**/*.war'
-}
-
-
 def tomcatdeploy(jobname,ip,context)
 {
     sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
